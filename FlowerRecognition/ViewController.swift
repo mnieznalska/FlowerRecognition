fd0e4ff9e690ba5eb8c6ff6168776a79d8371b58
Magdalena <magdalena@magdalenas-mini.fritz.box>
2018-07-27 12:02:44 +0200

    Basic UI implementation ImagePicker Implementation Flower detection functionality implemented

--- FlowerRecognition/ViewController.swift
+++ FlowerRecognition/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            
+            imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            detect(flowerImage: ciImage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func detect(flowerImage: CIImage) {
+        
+        guard let flowerModel = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            
+            fatalError("Loading CoreML Model Failed.")
+            
+        }
+        
+        let detectionRequest = VNCoreMLRequest(model: flowerModel) { (detectionRequest, error) in
+            
+            guard let detectionResult = detectionRequest.results as? [VNClassificationObservation] else {
+                
+                fatalError("Model failed to process image.")
+                
+            }
+            
+            self.navigationItem.title = detectionResult.first?.identifier.capitalized
+        
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        
+        do {
+            try handler.perform([detectionRequest])
+        }
+        catch {
+            print("Error detecting Image \(error)")
+        }
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
